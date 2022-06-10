24e86cbe66f391eac3ca91baf644a12eb2d55ec3
fengmeili <18022728612@163.com>
2022-06-10 22:21:11 +0800

    第几次

--- vue-admin-template-master/src/utils/myrequest.js
+++ vue-admin-template-master/src/utils/myrequest.js
@@ -5,7 +5,7 @@ import { getToken } from '@/utils/auth'
 
 // create an axios instance
 const service = axios.create({
-  baseURL: 'http://192.168.31.18:80/api', // url = base url + request url
+  baseURL: 'https://aqueous-forest-94483.herokuapp.com/api', // url = base url + request url
   // withCredentials: true, // send cookies when cross-domain requests
   timeout: 5000, // request timeout
 })
